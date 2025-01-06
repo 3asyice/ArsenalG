2c53a43e1ae51c840224b0cd730ab02e07677308
Easy×͜×Ice <116710976+3asyice@users.noreply.github.com>
2025-01-05 21:18:04 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('assets/videos.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(videos => {
+      const section = document.querySelector('.section__one');
+
+      videos.forEach(video => {
+        section.appendChild(createVideoElement(video));
+      });
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+
+  function createVideoElement(video) {
+    const container = document.createElement('div');
+
+    const hr = document.createElement('hr');
+    const h2 = document.createElement('h2');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+    const iframe = document.createElement('iframe');
+
+    h2.textContent = video.title;
+    a.target = `iframe${video.title.replace(/\s+/g, '')}`;
+    a.href = video.url;
+
+    img.src = video.img;
+    img.alt = video.title;
+    img.width = "100%";
+    img.height = "250";
+
+    iframe.width = "100%";
+    iframe.height = "300";
+    iframe.allowFullscreen = true;
+    iframe.name = `iframe${video.title.replace(/\s+/g, '')}`;
+    iframe.src = "";
+    iframe.frameBorder = "0";
+
+    a.appendChild(img);
+    container.append(hr, h2, a, iframe); // Adiciona todos os elementos ao container
+    return container; // Retorna o container que contém todos os elementos
+  }
+});
