cc9d3b5884d4a255f7c256ed0d1cc4b3836726d5
Easy×͜×Ice <116710976+3asyice@users.noreply.github.com>
2025-01-11 15:40:07 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -29,14 +29,17 @@ document.addEventListener("DOMContentLoaded", function() {
     h2.textContent = video.title;
     a.target = `iframe${video.title.replace(/\s+/g, '')}`;
     a.href = video.url;
+    h2.style.fontFamily = "Jacquard 24 Charted";
 
     img.src = video.img;
     img.alt = video.title;
+    img.border = "1px solid #00ff00";
     img.width = "100%";
     img.height = "250";
 
     iframe.width = "100%";
-    iframe.height = "300";
+    iframe.height = "250";
+    iframe.border = "1px solid #00ff00";
     iframe.allowFullscreen = true;
     iframe.name = `iframe${video.title.replace(/\s+/g, '')}`;
     iframe.src = "";
