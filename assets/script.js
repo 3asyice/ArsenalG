4dfb8892401cb26ef897d57259dc89b4ecabf0f3
3asyice <guilhermeabreu294@gmail.com>
2025-05-06 17:22:28 -0300

    update

--- assets/script.js
+++ assets/script.js
@@ -18,35 +18,39 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
   function createVideoElement(video) {
-    const container = document.createElement('div');
-
-    const hr = document.createElement('hr');
-    const h2 = document.createElement('h2');
-    const a = document.createElement('a');
-    const img = document.createElement('img');
-    const iframe = document.createElement('iframe');
-
-    h2.textContent = video.title;
-    a.target = `iframe${video.title.replace(/\s+/g, '')}`;
-    a.href = video.url;
-    h2.style.fontFamily = "Jacquard 24 Charted";
-
-    img.src = video.img;
-    img.alt = video.title;
-    img.border = "1px solid #00ff00";
-    img.width = "100%";
-    img.height = "250";
-
-    iframe.width = "100%";
-    iframe.height = "250";
-    iframe.border = "1px solid #00ff00";
-    iframe.allowFullscreen = true;
-    iframe.name = `iframe${video.title.replace(/\s+/g, '')}`;
-    iframe.src = "";
-    iframe.frameBorder = "0";
-
-    a.appendChild(img);
-    container.append(hr, h2, a, iframe); // Adiciona todos os elementos ao container
-    return container; // Retorna o container que contém todos os elementos
-  }
+  const container = document.createElement('div');
+  
+  const hr = document.createElement('hr');
+  const h2 = document.createElement('h2');
+  const a = document.createElement('a');
+  const img = document.createElement('img');
+  const iframe = document.createElement('iframe');
+  
+  h2.textContent = video.title;
+  h2.style.fontFamily = "Jacquard 24 Charted";
+  
+  img.src = video.img;
+  img.alt = video.title;
+  img.border = "1px solid #00ff00";
+  img.width = "100%";
+  img.height = "250";
+  
+  // Defina o href do link para o URL do vídeo
+  a.href = video.url;
+  a.target = "_blank"; // Abre o link em uma nova aba
+  
+  // Adicione a imagem ao link
+  a.appendChild(img);
+  
+  iframe.width = "100%";
+  iframe.height = "250";
+  iframe.border = "1px solid #00ff00";
+  iframe.allowFullscreen = true;
+  iframe.name = `iframe${video.title.replace(/\s+/g, '')}`;
+  iframe.src = "";
+  iframe.frameBorder = "0";
+  
+  container.append(hr, h2, a, iframe); // Adiciona todos os elementos ao container
+  return container; // Retorna o container que contém todos os elementos
+}
 });
