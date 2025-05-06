61c7306ce16ffc7a9d3cf45e323232612482d291
3asyice <guilhermeabreu294@gmail.com>
2025-05-06 17:59:04 -0300

    update

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
       return response.json();
     })
     .then(videos => {
-      const section = document.querySelector('.section__one');
+const section = document.querySelector('.section__one');
 
       videos.forEach(video => {
         section.appendChild(createVideoElement(video));
@@ -25,32 +25,31 @@ document.addEventListener("DOMContentLoaded", function() {
   const a = document.createElement('a');
   const img = document.createElement('img');
   const iframe = document.createElement('iframe');
+  const download = document.createElement('a');
   
   h2.textContent = video.title;
-  h2.style.fontFamily = "Jacquard 24 Charted";
+  h2.style.fontFamily = "sans-serif";
+  h2.style.fontWeight = '600';
+  h2.style.fontSize = '18px';
+// ou == Jacquard 24 Charted 
   
   img.src = video.img;
   img.alt = video.title;
-  img.border = "1px solid #00ff00";
+  img.border = "1px solid #ff0000";
   img.width = "100%";
   img.height = "250";
-  
-  // Defina o href do link para o URL do vídeo
   a.href = video.url;
-  a.target = "_blank"; // Abre o link em uma nova aba
-  
-  // Adicione a imagem ao link
+  a.target = "_blank";
   a.appendChild(img);
-  
   iframe.width = "0%";
   iframe.height = "0";
-  iframe.border = "1px solid #00ff00";
+  iframe.border = "1px solid #ff0000";
   iframe.allowFullscreen = true;
   iframe.name = `iframe${video.title.replace(/\s+/g, '')}`;
   iframe.src = "";
   iframe.frameBorder = "0";
   
-  container.append(hr, h2, a, iframe); // Adiciona todos os elementos ao container
-  return container; // Retorna o container que contém todos os elementos
+  container.append(hr, h2, a, iframe);
+  return container;
 }
 });
